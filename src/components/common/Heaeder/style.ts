2d3afc83fad589dd76ab54edcf4959205d557123
boksooni <8545652@naver.com>
2022-11-03 20:36:25 +0900

    design/Header 높이 수정/#13

--- src/components/common/Heaeder/style.ts
+++ src/components/common/Heaeder/style.ts
@@ -1,9 +1,9 @@
 import styled from 'styled-components';
 
 export const Wrapper = styled.div`
-  background-color: #fff;
+  background-color: pink;
   width: 100%;
-  height: 140px;
+  height: 80px;
   display: flex;
   align-items: center;
 `;
