a50de8c859eecd74bd7827a884e41b43a18a40ca
boksooni <8545652@naver.com>
2022-11-16 22:37:10 +0900

    design/globalStyle 추가/#36
    Co-authored-by: gyduddl <gyduddl4889@naver.com>
    Co-authored-by: SEONWOO LEE <dkel3slek@users.noreply.github.com>

--- src/styles/globalStyles.ts
+++ src/styles/globalStyles.ts
@@ -3,22 +3,29 @@ import { createGlobalStyle } from 'styled-components';
 export default createGlobalStyle`
 * {
   font-family: 'Pretendard Variable' !important;
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
 }
 html {
   font-size: 62.5%;
 }
-body {
-  margin: 0;
-}
 button {
   border: none;
   background: none;
-  padding: 0;
   cursor: pointer;
 }
 a {
   text-decoration: none;
 }
-
+ul {
+    list-style-type: none;
+    margin-block-start: 0;
+    margin-block-end: 0;
+    padding-inline-start: 0;
+}
+input {
+  border: none;
+}
 
 `;
