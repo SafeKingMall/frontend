17a0567c435cd3854c11f584e6567c1580ef6ecb
이선우 <dkel3slek@naver.com>
2022-12-02 03:44:15 +0900

    design/list와 총주문금액/#47

--- src/pages/Carts/index.tsx
+++ src/pages/Carts/index.tsx
@@ -1,14 +1,68 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import * as S from './style';
 import { OrdersStep } from '../../components/shopping/OrdersStep';
-import { OrdersList } from '../../components/shopping/OrdersList';
+import { CartsList } from '../../components/shopping/CartsList';
+import { TotalPrice } from '../../components/shopping/TotalPrice';
+import { NoCartsItem } from '../../components/shopping/NoCartsItem';
+
+//테스트 데이터
+const data: any = [
+  {
+    itemId: 1,
+    name: '안전화1',
+    price: 90000,
+    categoryName: '중대사고예방',
+    description: '상품상세설명 블라블라',
+    count: 1,
+  },
+  {
+    itemId: 3,
+    name: '코팅장갑',
+    price: 1000,
+    categoryName: '화재사고예방',
+    description: '상품상세설명 블라블라',
+    count: 2,
+  },
+  {
+    itemId: 5,
+    name: '축압식 분말 소화기 (1.5kg)',
+    price: 13900,
+    categoryName: '누출사고예방',
+    description: '상품상세설명 블라블라',
+    count: 1,
+  },
+];
+
+// const data: any = [];
 
 export const Carts = () => {
   const stepTitle = '장바구니';
-  return (
-    <S.Container>
-      <OrdersStep stepTitle={stepTitle} />
-      <OrdersList />
-    </S.Container>
-  );
+  const navigate = useNavigate();
+  const [resultList, setResultList] = useState(data);
+  const selectPurchase = () => {
+    if (resultList.length === 0) {
+      alert('선택상품이 없습니다.');
+    } else {
+      navigate('/Orders');
+    }
+  };
+  const itemInCart =
+    data.length === 0 ? (
+      <S.Container>
+        <NoCartsItem />
+      </S.Container>
+    ) : (
+      <S.Container>
+        <OrdersStep stepTitle={stepTitle} />
+        <CartsList data={data} setResultList={setResultList} />
+        <TotalPrice resultList={resultList} />
+        <S.BtnContainer>
+          <S.WhiteBtn onClick={() => navigate('/')}>쇼핑하러 가기</S.WhiteBtn>
+          <S.WhiteBtn onClick={() => selectPurchase()}>선택상품구매</S.WhiteBtn>
+          <S.GreenBtn onClick={() => navigate('/Orders')}>전체상품구매</S.GreenBtn>
+        </S.BtnContainer>
+      </S.Container>
+    );
+  return itemInCart;
 };
--- src/pages/Carts/style.ts
+++ src/pages/Carts/style.ts
@@ -4,3 +4,27 @@ export const Container = styled.div`
   width: 100%;
   height: 100%;
 `;
+export const BtnContainer = styled.div`
+  ${({ theme }) => theme.common.flexCenter};
+  width: 100%;
+  height: 8rem;
+  margin-top: 4.8rem;
+  margin-bottom: 16rem;
+`;
+export const Btn = styled.button`
+  width: 28rem;
+  height: 100%;
+  border-radius: 10rem;
+  font-size: 2.4rem;
+  font-weight: 700;
+`;
+export const WhiteBtn = styled(Btn)`
+  border: 0.2rem solid ${({ theme }) => theme.palette.green};
+  background-color: #ffffff;
+  color: ${({ theme }) => theme.palette.green};
+  margin-right: 1.6rem;
+`;
+export const GreenBtn = styled(Btn)`
+  background-color: ${({ theme }) => theme.palette.green};
+  color: #ffffff;
+`;
