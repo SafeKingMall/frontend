135e6a72c57a66423a8f0d7befb9de66784455a8
gyduddl <gyduddl4889@naver.com>
2023-01-03 16:12:48 +0900

    design/EditMenu디자인수정/#79

--- src/components/Edit/EditMenu/index.tsx
+++ src/components/Edit/EditMenu/index.tsx
@@ -1,13 +1,43 @@
 import React from 'react';
 import * as S from './style';
+import { useEffect } from 'react';
 
 export const EditMenu = () => {
+  const location = window.location.pathname;
+
+  useEffect(() => {
+    console.log(location);
+  }, [location]);
+
+  const numberColor = '#289951';
+
   return (
     <S.Wrapper>
-      <S.logo />
-      <S.StyledLink to='/admin-member'>회원관리</S.StyledLink>
-      <S.StyledLink to='/admin-item'>상품관리</S.StyledLink>
-      <S.StyledLink to='/admin-order'>주문관리</S.StyledLink>
+      <S.StyledLink
+        to='/admin-member'
+        style={{ color: location === '/admin-member' ? numberColor : '' }}
+      >
+        회원관리
+      </S.StyledLink>
+      <S.StyledLink
+        to='/admin-item'
+        style={{ color: location === '/admin-item' ? numberColor : '' }}
+      >
+        상품관리
+      </S.StyledLink>
+      <S.StyledLink
+        to='/admin-order'
+        style={{ color: location === '/admin-order' ? numberColor : '' }}
+      >
+        주문관리
+      </S.StyledLink>
+
+      <S.StyledLink
+        to='/admin-category'
+        style={{ color: location === '/admin-category' ? numberColor : '' }}
+      >
+        카테코리관리
+      </S.StyledLink>
     </S.Wrapper>
   );
 };
