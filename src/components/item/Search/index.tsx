d6b9dc16ca50836e3cce24784f57a4532c93ef9a
이선우 <dkel3slek@naver.com>
2023-01-12 23:01:16 +0900

    api/ItemList/#86

--- src/components/item/Search/index.tsx
+++ src/components/item/Search/index.tsx
@@ -1,45 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
 import * as S from './style';
 import { AiOutlineSearch } from 'react-icons/ai';
 
 export const Search = (props: any) => {
-  // let data = props.data;
-  // const [sortState, setSortState] = useState('최신 순');
-  // const sortChange = (e: any) => {
-  //   setSortState(e.target.value);
-  // };
-  // if (sortState === '최신 순') {
-  //   data = [...data].reverse();
-  // } else if (sortState === '이름 순') {
-  //   data = [...data].sort((a, b) => (a.name < b.name ? -1 : a.name > b.name ? 1 : 0));
-  // } else if (sortState === '가격 낮은 순') {
-  //   data = [...data].sort((a, b) => a.price - b.price);
-  // } else if (sortState === '가격 높은 순') {
-  //   data = [...data].sort((a, b) => b.price - a.price);
-  // }
+  const setSort = props.setSort;
+  const setSearchItem = props.setSearchItem;
+  const [searchText, setSearchText] = useState('');
+  const sortChange = (e: any) => {
+    if (e.target.value === '최신 순') {
+      setSort(`sort=createDate,desc&sort=name,asc`);
+    } else if (e.target.value === '이름 순') {
+      setSort(`sort=name,asc&sort=id,asc`);
+    } else if (e.target.value === '가격 낮은 순') {
+      setSort(`sort=price,asc&sort=name,asc`);
+    } else if (e.target.value === '가격 높은 순') {
+      setSort(`sort=price,desc&sort=name,asc`);
+    }
+  };
+  const onChangeText = (e: any) => {
+    setSearchText(e.target.value);
+  };
+  const onKeyDownEnter = (e: any) => {
+    if (e.key === 'Enter') {
+      search();
+      e.target.blur();
+    }
+  };
+  const search = () => {
+    setSearchItem(searchText);
+  };
   return (
     <S.Container>
       <S.SearchArea>
-        <S.Select>
+        <S.Select onChange={(e) => sortChange(e)}>
           <option value='최신 순'>최신 순</option>
           <option value='이름 순'>이름 순</option>
           <option value='가격 낮은 순'>가격 낮은 순</option>
           <option value='가격 높은 순'>가격 높은 순</option>
         </S.Select>
-        <S.SearchBar type='text' placeholder='검색어를 입력해주세요'></S.SearchBar>
-        <button
-          style={{
-            width: '4rem',
-            height: '4rem',
-            backgroundColor: '#289951',
-            position: 'absolute',
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'center',
-          }}
-        >
+        <S.SearchBar
+          type='text'
+          placeholder='검색어를 입력해주세요'
+          onChange={(e) => onChangeText(e)}
+          onKeyDown={(e) => onKeyDownEnter(e)}
+        ></S.SearchBar>
+        <S.SearchBtn onClick={() => search()}>
           <AiOutlineSearch size='2.8rem' color='#ffffff' />
-        </button>
+        </S.SearchBtn>
       </S.SearchArea>
     </S.Container>
   );
