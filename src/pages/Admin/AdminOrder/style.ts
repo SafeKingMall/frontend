2f2cb3cc78a953338f61000b9b0ff62ae31c7512
gyduddl <gyduddl4889@naver.com>
2023-01-16 18:42:21 +0900

    design/adminorder페이지제작/#98

---
+++ src/pages/Admin/AdminOrder/style.ts
@@ -0,0 +1,49 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  display: flex;
+  //   width: 50%;
+  //   height: 100%;
+`;
+
+export const Wrapper = styled.div`
+  /* width: 144rem; */
+  display: flex;
+  width: 100%;
+  height: 100%;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const AdminMembertitle = styled.div`
+  // height: 100%;
+  width: 144rem;
+  margin-top: 2.4rem;
+  display: flex;
+  flex-direction: row;
+  flex-wrap: nowrap;
+  flex-wrap: wrap;
+  background-color: ${({ theme }) => theme.palette.darkgreen};
+  align-items: end;
+  //   justify-content: space-between;
+  padding: 2.4rem 5rem 2.4rem 5.4rem;
+  align-items: center;
+  border-top: 0.3rem solid ${({ theme }) => theme.palette.green};
+  border-bottom: 0.3rem solid ${({ theme }) => theme.palette.txtgray};
+  font-size: 2.4rem;
+  div {
+    &:nth-child(1) {
+      flex-grow: 2.1;
+    }
+    &:nth-child(2) {
+      flex-grow: 2.7;
+    }
+    &:nth-child(3) {
+      flex-grow: 2;
+    }
+    &:nth-child(4) {
+      flex-grow: 0;
+    }
+  }
+`;
