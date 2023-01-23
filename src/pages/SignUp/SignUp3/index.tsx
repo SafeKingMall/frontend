32d3f6b6c1dc7c231ee98eb94286c6ac3bdff68b
boksooni <8545652@naver.com>
2023-01-24 05:17:24 +0900

    feat/SignUp3/#97

--- src/pages/SignUp/SignUp3/index.tsx
+++ src/pages/SignUp/SignUp3/index.tsx
@@ -1,5 +1,362 @@
-import React from 'react';
+/* eslint-disable no-unused-vars */
+import React, { useState, useRef, useEffect } from 'react';
+import { Footer } from '../../../components/common/Footer';
+import { Header } from '../../../components/common/Header';
+import * as S from './style';
+import DaumPostcode from 'react-daum-postcode';
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
+
+interface SignUpForm3 {
+  company: string;
+  saupja: string;
+  beobin: string;
+  ceo: string;
+  number: string;
+  autoClose: boolean;
+}
 
 export const SignUp3 = () => {
-  return <div>SignUp3</div>;
+  const navigate = useNavigate();
+  const [company, SetCompany] = useState<string>('');
+  const [saupja, setSaupja] = useState<string>('');
+  const saupjaRef = useRef<any>();
+  const [beobin, setBeobin] = useState<string>('');
+  const beobinRef = useRef<any>();
+  const [ceo, setCeo] = useState<string>('');
+  const [zip, setZip] = useState<string>('');
+  const [basic, setBasic] = useState<string>('');
+  const [detail, setDetail] = useState<string>('');
+  const [number, setNumber] = useState<string>('');
+  //유효성 검사
+  const [isCompany, setIsCompany] = useState<boolean>(false);
+  const [isSaupja, setIsSaupja] = useState<boolean>(false);
+  const [isBeobin, setIsBeobin] = useState<boolean>(false);
+  const [isZip, setIsZip] = useState<boolean>(false);
+  const [isBasic, setIsBasic] = useState<boolean>(false);
+  const [isDetail, setIsDetail] = useState<boolean>(false);
+
+  const [isCeo, setIsCeo] = useState<boolean>(false);
+  const [isNumber, setIsNumber] = useState<boolean>(false);
+  //에러메세지
+  const [companyMsg, SetCompanyMsg] = useState<string>('');
+  const [saupjaMsg, setSaupjaMsg] = useState<string>('');
+  const [beobinMsg, setBeobinMsg] = useState<string>('');
+  const [ceoMsg, setCeoMsg] = useState<string>('');
+  const [detailMsg, setDetailMsg] = useState<string>('');
+  const [numberMsg, setNumberMsg] = useState<string>('');
+
+  //회사명 한글영어숫자만 특문:한영숫으로만
+  const onCompany = (e: React.ChangeEvent<HTMLInputElement>) => {
+    e.preventDefault();
+    const companyCurrent = e.target.value;
+    SetCompany(companyCurrent);
+    const companyRegex = /[^a-z|A-Z|0-9|ㄱ-ㅎ|가-힣]/g;
+
+    if (companyCurrent === '') {
+      SetCompanyMsg('회사명을 입력해주세요.');
+      setIsCompany(false);
+    } else if (companyRegex.test(companyCurrent)) {
+      SetCompanyMsg('한글/영어/숫자로만 입력해주세요.');
+      setIsCompany(false);
+    } else {
+      SetCompanyMsg('');
+      setIsCompany(true);
+    }
+  };
+  //사업자 (000-00-00000) 정규식처리, 10자리 초과/미만
+  const onSaupja = (e: React.ChangeEvent<HTMLInputElement>) => {
+    e.preventDefault();
+    //사업자 인풋 내 하이픈 자동 생성
+    const autoSaupja = saupjaRef.current.value.replace(/\D+/g, '');
+    const saupjaLength = 10;
+    let result;
+    result = '';
+    for (let i = 0; i < autoSaupja.length && i < saupjaLength; i++) {
+      switch (i) {
+        case 3:
+          result += '-';
+          break;
+        case 5:
+          result += '-';
+          break;
+
+        default:
+          break;
+      }
+      result += autoSaupja[i];
+    }
+    saupjaRef.current.value = result;
+    setSaupja(e.target.value);
+    // console.log('dfs', saupja);
+    const saupjaRegex = /^[0-9]{3}-[0-9]{2}-[0-9]{5}$/;
+    if (result === '') {
+      setSaupjaMsg('사업자 등록번호를 입력해주세요');
+      setIsSaupja(false);
+    } else if (saupjaRegex.test(result)) {
+      // console.log('유효성 통과');
+      setSaupjaMsg('');
+      setIsSaupja(true);
+    }
+  };
+
+  //법인 (00000-0000000) 정규식처리, 10자리 초과/미만
+  const onBeobin = (e: React.ChangeEvent<HTMLInputElement>) => {
+    e.preventDefault();
+    //법인 인풋 내 하이픈 자동 생성
+    const autoBeobin = beobinRef.current.value.replace(/\D+/g, '');
+    const beobinLength = 12;
+    let result;
+    result = '';
+    for (let i = 0; i < autoBeobin.length && i < beobinLength; i++) {
+      switch (i) {
+        case 5:
+          result += '-';
+          break;
+
+        default:
+          break;
+      }
+      result += autoBeobin[i];
+    }
+    beobinRef.current.value = result;
+    setBeobin(e.target.value);
+    // console.log('dfs', beobin);
+    const beobinRegex = /^([0-9]{5})-([0-9]{7})$/;
+    if (result === '') {
+      setBeobinMsg('법인등록번호를 입력해주세요');
+      setIsBeobin(false);
+    } else if (beobinRegex.test(result)) {
+      // console.log('법인 유효성 통과');
+      setBeobinMsg('');
+      setIsBeobin(true);
+    }
+  };
+  //대표명
+  const onCeo = (e: React.ChangeEvent<HTMLInputElement>) => {
+    e.preventDefault();
+    const ceoCurrent = e.target.value;
+    setCeo(ceoCurrent);
+
+    if (ceoCurrent === '') {
+      setCeoMsg('대표명을 입력해주세요');
+      setIsCeo(false);
+    } else {
+      setCeoMsg('');
+      setIsCeo(true);
+    }
+  };
+  //다음주소
+  const [openPost, setOpenPost] = useState<boolean>(false);
+  const closeRef = useRef<any>(null);
+  const [zipText, setZipText] = useState<string>('');
+  const [basicText, setBasicText] = useState<string>('');
+
+  const handleClickOutside = (e: React.MouseEvent) => {
+    setOpenPost(false);
+  };
+
+  const postCodeStyle = {
+    // width: '700px',
+    // height: '700px',
+    // padding: '7px',
+    // backgroundColor: '#ffffff',
+    // borderRadius: '15px',
+  };
+  const onOpenPost = () => {
+    setOpenPost(true);
+  };
+  const onClosePost = (e: any) => {
+    setOpenPost(false);
+  };
+  const onCompletePost = (data: any) => {
+    console.log(data);
+    let fullAddr = data.address;
+    let extraAddr = '';
+    let zipValue = '';
+    let basicValue = '';
+    if (data.addressType === 'R') {
+      if (data.bname !== '') {
+        extraAddr += data.bname;
+      }
+      if (data.buildingName !== '') {
+        extraAddr += extraAddr !== '' ? `, ${data.buildingName}` : data.buildingName;
+      }
+      fullAddr += extraAddr !== '' ? ` (${extraAddr})` : '';
+      console.log(fullAddr);
+      zipValue = data.zonecode;
+      console.log(zipValue, 'sdfasdfsdf');
+      //우편번호
+      setZipText(zipValue);
+      setIsZip(true);
+      //기본주소
+      basicValue = fullAddr;
+      setBasicText(basicValue);
+      setIsBasic(true);
+    }
+  };
+  //상세주소
+  const onDetail = (e: React.ChangeEvent<HTMLInputElement>) => {
+    e.preventDefault();
+    const detailCurrent = e.target.value;
+    setDetail(detailCurrent);
+    const detailRegex = /[^a-z|A-Z|0-9|ㄱ-ㅎ|가-힣]/g;
+
+    if (detailCurrent === '') {
+      setDetailMsg('상세주소를 입력해주세요');
+      setIsDetail(false);
+    } else if (detailRegex.test(detailCurrent)) {
+      setDetailMsg('한글/영어/숫자로만 입력해주세요');
+      setIsDetail(false);
+    } else {
+      setDetailMsg('');
+      setIsDetail(true);
+    }
+  };
+  //연락처
+  const onNumber = (e: React.ChangeEvent<HTMLInputElement>) => {
+    e.preventDefault();
+    const numberCurrent = e.target.value;
+    setNumber(String(numberCurrent));
+
+    if (numberCurrent === '') {
+      setNumberMsg('연락처를 입력해주세요');
+      setIsNumber(false);
+    } else if (numberCurrent.length > 11) {
+      setNumberMsg('최대 11자리까지 입력가능합니다');
+      setIsNumber(false);
+    } else {
+      setNumberMsg('');
+      setIsNumber(true);
+    }
+  };
+  //다음 버튼 disable = 회사명, 사업자, 법인, 우편벙호, 기본주소, 상세주소
+  const disabled = !(isCompany && isSaupja && isBeobin && isZip && isBasic && isDetail);
+  const onSubmit = async (e: any) => {
+    e.preventDefault();
+    console.log('com:', company, 'saup:', saupja, 'beob:', beobin, zipText, basicText, detail);
+
+    try {
+      await axios
+        .post('http://safekingmall.com/api/v1/signup/memberInfo/1', {
+          companyName: company,
+          companyRegistrationNumber: saupja,
+          corporateRegistrationNumber: beobin,
+          representativeName: ceo,
+          zipcode: zipText,
+          basicAddress: basicText,
+          detailedAddress: detail,
+          contact: number,
+        })
+        .then((res) => {
+          console.log(res);
+          if (res.status === 200) {
+            navigate('/sign-up4');
+          }
+        });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  const test = (e: any) => {
+    e.preventDefault();
+    console.log(isCompany, isSaupja, isBeobin, isZip, isBasic, isDetail);
+  };
+  return (
+    <>
+      <Header />
+      <button onClick={test}>test</button>
+      <S.Container>
+        <S.Wrapper>
+          <S.ProcessBar></S.ProcessBar>
+          <S.Mid>회원정보 입력</S.Mid>
+          <S.InputContainer>
+            <S.InputWrapper>
+              <label>
+                <span>*</span> &nbsp; 회사명
+              </label>
+              <input placeholder='* 표시는 필수 입력 정보입니다' onChange={onCompany} />
+              <S.ErrMsg>{companyMsg}</S.ErrMsg>
+            </S.InputWrapper>
+            <S.InputWrapper>
+              <label>
+                <span>*</span> &nbsp; 사업자등록번호
+              </label>
+              <input
+                placeholder='* 표시는 필수 입력 정보입니다'
+                onChange={onSaupja}
+                value={saupja}
+                ref={saupjaRef}
+                max='10'
+              />
+              <S.ErrMsg>{saupjaMsg}</S.ErrMsg>
+            </S.InputWrapper>
+            <S.InputWrapper>
+              <label>
+                <span>*</span> &nbsp; 법인등록번호
+              </label>
+              <input
+                placeholder='* 표시는 필수 입력 정보입니다'
+                onChange={onBeobin}
+                value={beobin}
+                ref={beobinRef}
+                max='12'
+              />
+              <S.ErrMsg>{beobinMsg}</S.ErrMsg>
+            </S.InputWrapper>
+            <S.InputWrapper>
+              <label>&ensp; &nbsp; 대표명</label>
+              <input onChange={onCeo} />
+              <S.ErrMsg>{ceoMsg}</S.ErrMsg>
+            </S.InputWrapper>
+            <S.InputWrapper>
+              <label>
+                <span>*</span> &nbsp; 우편번호
+              </label>
+              <input
+                value={zipText}
+                placeholder='주소찾기를 눌러주세요.'
+                onChange={(e) => setZip(e.target.value)}
+              />
+              <button onClick={onOpenPost}>주소찾기</button>
+            </S.InputWrapper>
+            {openPost && (
+              <S.PostModal onClick={onClosePost}>
+                <DaumPostcode onComplete={onCompletePost} autoClose={true}></DaumPostcode>
+              </S.PostModal>
+            )}
+            <S.InputWrapper>
+              <label>
+                <span>*</span> &nbsp; 기본주소
+              </label>
+              <input
+                placeholder='* 표시는 필수 입력 정보입니다'
+                value={basicText}
+                onChange={(e) => setBasic(e.target.value)}
+              />
+            </S.InputWrapper>
+            <S.InputWrapper>
+              <label>
+                <span>*</span> &nbsp; 상세주소
+              </label>
+              <input placeholder='* 표시는 필수 입력 정보입니다' onChange={onDetail} />
+              <S.ErrMsg>{detailMsg}</S.ErrMsg>
+            </S.InputWrapper>
+            <S.InputWrapper>
+              <label>&ensp; &nbsp; 연락처</label>
+              <input onChange={onNumber} max='11' />
+              <S.ErrMsg>{numberMsg}</S.ErrMsg>
+            </S.InputWrapper>
+          </S.InputContainer>
+          <S.BtnWrapper>
+            <button onClick={() => navigate(-1)}>이전</button>
+            <button disabled={disabled} onClick={onSubmit}>
+              다음
+            </button>
+          </S.BtnWrapper>
+        </S.Wrapper>
+      </S.Container>
+      <Footer />
+    </>
+  );
 };
---
+++ src/pages/SignUp/SignUp3/style.ts
@@ -0,0 +1,189 @@
+import styled from 'styled-components';
+export const Container = styled.div`
+  ${({ theme }) => theme.common.flexCenter};
+`;
+export const Wrapper = styled.div`
+  background-color: #fff;
+  width: 129.8rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  /* height: 109.6rem; */
+`;
+export const ProcessBar = styled.div`
+  margin: 5.6rem 0 0;
+  font-size: 2.4rem;
+  font-weight: 400;
+  line-height: 3rem;
+  width: 40rem;
+  height: 9.9rem;
+  background-image: url('https://user-images.githubusercontent.com/91241596/213253660-d5f39637-aafc-43b1-b6a9-abb3978e5c15.png');
+`;
+export const Mid = styled.div`
+  margin: 7.2rem 0 1.6rem;
+  font-weight: 700;
+  font-size: 24px;
+  line-height: 29px;
+  color: #289951;
+  display: block;
+  align-items: left;
+`;
+export const InputContainer = styled.div`
+  background-color: #fff;
+  width: 64rem;
+  height: 70.4rem;
+  border: 1px solid #efeff1;
+  /* border: 0.1rem solid #fff; */
+  border-radius: 1rem;
+  & > div:last-child {
+    &::after {
+      display: none;
+    }
+  }
+`;
+export const InputWrapper = styled.div`
+  /* background-color: mistyrose; */
+  height: 8.8rem;
+  position: relative;
+  & > label {
+    /* background-color: thistle; */
+    display: inline-block;
+    width: 20rem;
+    height: 17rem;
+    font-size: 1.8rem;
+    font-weight: 700;
+    line-height: 2.2rem;
+    padding: 3.4rem 0 0 3rem;
+    > span {
+      font-weight: 700;
+      font-size: 12px;
+      line-height: 15px;
+      color: #289951;
+    }
+  }
+  & > button {
+    background-color: #fff;
+    width: 10.4rem;
+    height: 4rem;
+    border: 0.1rem solid #289951;
+    border-radius: 1rem;
+    color: #289951;
+    font-size: 1.6rem;
+    font-weight: 400;
+    line-height: 1.9rem;
+    position: absolute;
+    top: 2.5rem;
+    right: 2.4rem;
+  }
+  & > input {
+    background-color: #fff;
+    font-size: 1.8rem;
+    font-weight: 400;
+    line-height: 2.2rem;
+    padding-left: 2.4rem;
+    width: 42rem;
+  }
+  & > p {
+    color: red;
+    font-weight: 400;
+    font-size: 16px;
+    line-height: 19px;
+    position: absolute;
+    top: 3.5rem;
+    right: 11.5rem;
+  }
+  & > div {
+    background-color: lavenderblush;
+    width: 66rem;
+    height: 5.8rem;
+    border-radius: 0.5rem;
+    display: flex;
+    align-items: center;
+    padding-left: 3.2rem;
+    font-size: 1.8rem;
+    font-weight: 400;
+    line-height: 2.2rem;
+    position: absolute;
+    bottom: 2rem;
+    left: 1.6rem;
+  }
+  &::before {
+    content: '';
+    position: absolute;
+    left: 20rem;
+    top: 3.4rem;
+    width: 0.1rem;
+    background-color: #efeff1;
+    width: 0.1rem;
+    height: 2.4rem;
+  }
+  &::after {
+    content: '';
+    position: absolute;
+    right: 0;
+    bottom: 0;
+    width: 0.1rem;
+    background-color: #efeff1;
+    width: 63.8rem;
+    height: 0.1rem;
+  }
+`;
+export const ErrMsg = styled.span`
+  font-weight: 400;
+  font-size: 14px;
+  line-height: 19px;
+  color: #ff003e;
+  position: absolute;
+  top: 6.1rem;
+  left: 22.4rem;
+`;
+export const BtnWrapper = styled.div`
+  margin-top: 4.8rem;
+  /* margin-bottom: 12rem; */
+  width: 64rem;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  > button {
+    background-color: #fff;
+    /* border: 0.1rem solid #000; */
+    border-radius: 10rem;
+    width: 31.2rem;
+    height: 8rem;
+    font-size: 2.4rem;
+    font-weight: 400;
+    line-height: 3rem;
+    :first-child {
+      border: 0.1rem solid #289951;
+      font-weight: 700;
+      font-size: 24px;
+      line-height: 29px;
+      /* identical to box height */
+
+      color: #289951;
+    }
+    :last-child {
+      background-color: #289951;
+      font-weight: 700;
+      font-size: 24px;
+      line-height: 29px;
+      /* identical to box height */
+
+      color: #ffffff;
+      :disabled {
+        background-color: #aaaaaa;
+      }
+    }
+  }
+`;
+export const PostModal = styled.div`
+  z-index: 10;
+  position: fixed;
+  width: 50rem;
+  height: 80rem;
+  top: 60%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  /* background-color: '#ffffff'; */
+`;
