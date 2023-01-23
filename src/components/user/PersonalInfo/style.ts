8c5e9729d226796c40302079280e2b1834d2d68f
boksooni <8545652@naver.com>
2023-01-24 05:15:47 +0900

    design/회원가입 약관 컴포넌트 생성/#97

---
+++ src/components/user/PersonalInfo/style.ts
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.div`
+  /* background-color: mistyrose; */
+  width: 64rem;
+  height: 27.2rem;
+  font-weight: 400;
+  font-size: 20px;
+  line-height: 24px;
+
+  color: #212121;
+  border: 1px solid #efeff1;
+  border-radius: 10px;
+  padding: 2rem 2.4rem 0 2.4rem;
+  white-space: pre-line;
+  overflow-y: scroll;
+`;
