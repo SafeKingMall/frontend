447417cf2f737ede45c388b751b6aecab264bc1e
boksooni <8545652@naver.com>
2023-01-26 06:33:06 +0900

    design/회원탈퇴 약관 컴포넌트/#110

---
+++ src/components/user/WithdrawInfo/style.ts
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.p`
+  /* background-color: lavenderblush; */
+  padding: 2.4rem;
+  width: 99.2rem;
+  height: 48.4rem;
+  font-weight: 400;
+  font-size: 16px;
+  line-height: 19px;
+
+  color: #000000;
+  border: 1px solid #efeff1;
+`;
