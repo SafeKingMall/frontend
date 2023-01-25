0a3a05960d56d6092e9b979a27fed325b000cc2b
boksooni <8545652@naver.com>
2023-01-26 06:28:09 +0900

    design/Footer/#110

--- src/components/common/Footer/index.tsx
+++ src/components/common/Footer/index.tsx
@@ -5,11 +5,13 @@ export const Footer = () => {
   return (
     <div>
       <S.Wrapper>
-        <S.Logo></S.Logo>
+        <S.Logo>
+          <div></div>
+        </S.Logo>
         <S.Text>
-          ㈜리크텍
-          <br /> Tel: (02) 6953-1170
-          <br /> Address: (07802) 서울시 강서구 공항대로 213, 보타닉파크타워-2 522호
+          <span>㈜리크텍</span>
+          <br /> <span>Tel</span> (02) 6953-1170
+          <br /> <span>Address</span> (07802) 서울시 강서구 공항대로 213, 보타닉파크타워-2 522호
           <br />
           <br />ⓒ safekingmall All rights reserved.
         </S.Text>
