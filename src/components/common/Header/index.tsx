53579db4217283fc29a353b8d5584e7e9f404dbb
이선우 <dkel3slek@naver.com>
2023-02-11 05:11:18 +0900

    design/Header/#172

--- src/components/common/Header/index.tsx
+++ src/components/common/Header/index.tsx
@@ -1,137 +1,71 @@
-/* eslint-disable no-unused-vars */
-import React from 'react';
+/* eslint-disable */
+import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import * as S from './style';
 import { AiOutlineUser, AiOutlineShopping } from 'react-icons/ai';
 import { FiLogIn, FiLogOut } from 'react-icons/fi';
 import { GrUserAdmin } from 'react-icons/gr';
 import { Cookies } from 'react-cookie';
+import Swal from 'sweetalert2';
+import withReactContent from 'sweetalert2-react-content';
 
 export const Header = () => {
   const navigate = useNavigate();
+  const swal = withReactContent(Swal);
   const cookies = new Cookies();
   const token = cookies.get('accessToken');
-  const admin = cookies.get('admin');
-  const user = cookies.get('user');
+  const loginUser = cookies.get('loginUser');
   const logout = () => {
-    cookies.remove('accessToken');
-    cookies.remove('refreshToken');
-    cookies.remove('admin');
-    cookies.remove('user');
-    navigate('/');
+    swal
+      .fire({
+        icon: 'question',
+        text: '로그아웃하시겠습니까?',
+        confirmButtonText: '확인',
+        confirmButtonColor: '#289951',
+        showCancelButton: true,
+        cancelButtonText: '취소',
+        width: 400,
+      })
+      .then((result) => {
+        if (result.isConfirmed) {
+          cookies.remove('accessToken');
+          cookies.remove('refreshToken');
+          cookies.remove('loginUser');
+          navigate('/');
+        }
+      });
   };
-
-  if (admin) {
-    return (
-      <>
-        <S.Wrapper>
-          <S.Logo onClick={() => navigate('/')}></S.Logo>
-          <S.Menu>
-            <S.MenuList>
-              <Link to='/itemlist-1'>예방상품리스트</Link>
-            </S.MenuList>
-            <S.MenuList>
-              <Link to='/notice'>공지사항</Link>
-            </S.MenuList>
-            <S.MenuList>
-              <Link to='/qna'>문의하기</Link>
-            </S.MenuList>
-            <S.MenuList>
-              <Link to='/estimate'>견적서 요청</Link>
-            </S.MenuList>
-          </S.Menu>
-          <S.MenuIcons>
-            <S.IconList>
-              <Link to='/admin-member'>
-                <GrUserAdmin />
-              </Link>
-            </S.IconList>
-            <S.IconList>
-              <Link to='/carts'>
-                <AiOutlineShopping />
-              </Link>
-            </S.IconList>
-            <S.IconList>
-              <FiLogOut onClick={logout} />
-            </S.IconList>
-          </S.MenuIcons>
-        </S.Wrapper>
-      </>
-    );
-  } else if (token || user) {
-    return (
-      <>
-        <S.Wrapper>
-          <S.Logo onClick={() => navigate('/')}></S.Logo>
-          <S.Menu>
-            <S.MenuList>
-              <Link to='/itemlist-1'>예방상품리스트</Link>
-            </S.MenuList>
-            <S.MenuList>
-              <Link to='/notice'>공지사항</Link>
-            </S.MenuList>
-            <S.MenuList>
-              <Link to='/qna'>문의하기</Link>
-            </S.MenuList>
-            <S.MenuList>
-              <Link to='/estimate'>견적서 요청</Link>
-            </S.MenuList>
-          </S.Menu>
-          <S.MenuIcons>
-            <S.IconList>
-              <Link to='/mypage'>
-                <AiOutlineUser />
-              </Link>
-            </S.IconList>
-            <S.IconList>
-              <Link to='/carts'>
-                <AiOutlineShopping />
-              </Link>
-            </S.IconList>
-            <S.IconList>
-              <FiLogOut onClick={logout} />
-            </S.IconList>
-          </S.MenuIcons>
-        </S.Wrapper>
-      </>
-    );
-  }
-
   return (
-    <>
-      <S.Wrapper>
-        <S.Logo onClick={() => navigate('/')}></S.Logo>
-        <S.Menu>
-          <S.MenuList>
-            <Link to='/itemlist-1'>예방상품리스트</Link>
-          </S.MenuList>
-          <S.MenuList>
-            <Link to='/notice'>공지사항</Link>
-          </S.MenuList>
-          <S.MenuList>
-            <Link to='/qna'>문의하기</Link>
-          </S.MenuList>
-          <S.MenuList>
-            <Link to='/estimate'>견적서 요청</Link>
-          </S.MenuList>
-        </S.Menu>
-        <S.MenuIcons>
-          {/* <S.IconList>
-            <Link to='/mypage'>
-              <AiOutlineUser />
-            </Link>
-          </S.IconList>
-          <S.IconList>
-            <Link to='/cart'>
-              <AiOutlineShopping />
-            </Link>
-          </S.IconList>
-          <S.IconList>
-            <AiOutlineSearch />
-          </S.IconList> */}
-          <FiLogIn onClick={() => navigate('/sign-in')} style={{ color: '#289951' }} />
-        </S.MenuIcons>
-      </S.Wrapper>
-    </>
+    <S.Container>
+      <S.LogoArea>
+        <S.Logo onClick={() => navigate('/')} />
+      </S.LogoArea>
+      <S.NavArea>
+        <S.Nav onClick={() => navigate('/itemlist-1')}>예방상품리스트</S.Nav>
+        <S.Nav onClick={() => navigate('/notice')}>공지사항</S.Nav>
+        <S.Nav onClick={() => navigate('/qna')}>문의하기</S.Nav>
+        <S.Nav onClick={() => navigate('/estimate')}>견적서 요청</S.Nav>
+      </S.NavArea>
+      <S.UserContentArea>
+        <S.UserContentBox>
+          {loginUser ? (
+            loginUser === 'admin') ? (
+            <S.GreenBtn onClick={() => navigate('/admin-member')}>관리자모드</S.GreenBtn>
+          ) : (
+            <S.IconBox>
+              <AiOutlineUser size='3rem' onClick={() => navigate('/mypage')} style={{ cursor: 'pointer' }} />
+              <AiOutlineShopping size='3rem' onClick={() => navigate('/carts')} style={{ cursor: 'pointer' }} />
+            </S.IconBox>
+          ) : (
+            ''
+          )}
+          {loginUser ? (
+            <S.GrayBtn onClick={() => logout()}>로그아웃</S.GrayBtn>
+          ) : (
+            <S.GreenBtn onClick={() => navigate('/sign-in')}>로그인</S.GreenBtn>
+          )}
+        </S.UserContentBox>
+      </S.UserContentArea>
+    </S.Container>
   );
 };
--- src/components/common/Header/style.ts
+++ src/components/common/Header/style.ts
@@ -1,59 +1,122 @@
 import styled from 'styled-components';
 
-export const Wrapper = styled.div`
-  /* background-color: pink; */
-  /* width: 100%; */
-  /* min-width: 180rem; */
-  padding: 0 1rem;
+// export const Wrapper = styled.div`
+//   /* background-color: pink; */
+//   /* width: 100%; */
+//   /* min-width: 180rem; */
+//   padding: 0 1rem;
+//   width: 100%;
+//   /* min-width: 130rem; */
+//   height: 8rem;
+//   display: flex;
+//   /* position: fixed;
+//   top: 0; */
+//   background-color: white;
+//   justify-content: space-between;
+//   align-items: center;
+// `;
+// export const Logo = styled.div`
+//   background-image: url(https://user-images.githubusercontent.com/91241596/212526764-a38ae3a1-34e0-4740-b6d0-741a4984431a.png);
+//   margin: 0 0 0 6rem;
+//   width: 15.6rem;
+//   height: 7.6rem;
+//   cursor: pointer;
+// `;
+// export const Menu = styled.ul`
+//   display: flex;
+//   font-size: 2rem;
+//   font-weight: 500;
+//   line-height: 2.4rem;
+//   /* justify-content: center; */
+//   text-align: center;
+//   margin: 0 auto;
+// `;
+// export const MenuList = styled.li`
+//   margin: 0 5rem;
+//   padding: 1rem 1rem;
+// `;
+
+// export const MenuIcons = styled.div`
+//   margin: 0 10rem 0 0;
+//   display: flex;
+//   font-size: 2.4rem;
+
+//   /* position: absolute;
+//   right: 10%; */
+// `;
+// export const IconList = styled.div`
+//   padding: 0 1rem;
+//   /* padding: 1rem 1rem; */
+// `;
+
+// //나중에 로그인버튼 바꿀떄
+// export const SignInBtn = styled.button`
+//   border-radius: 5rem;
+//   border: 0.1rem solid #999999;
+//   font-size: 2rem;
+//   height: 4.8rem;
+//   width: 10rem;
+// `;
+export const Container = styled.div`
   width: 100%;
-  /* min-width: 130rem; */
-  height: 8rem;
+  height: 10rem;
   display: flex;
-  /* position: fixed;
-  top: 0; */
-  background-color: white;
   justify-content: space-between;
   align-items: center;
-`;
-export const Logo = styled.div`
-  background-image: url(https://user-images.githubusercontent.com/91241596/212526764-a38ae3a1-34e0-4740-b6d0-741a4984431a.png);
-  margin: 0 0 0 6rem;
-  width: 15.6rem;
-  height: 7.6rem;
-  cursor: pointer;
-`;
-export const Menu = styled.ul`
-  display: flex;
   font-size: 2rem;
   font-weight: 500;
-  line-height: 2.4rem;
-  /* justify-content: center; */
-  text-align: center;
-  margin: 0 auto;
+  color: #212121;
 `;
-export const MenuList = styled.li`
-  margin: 0 5rem;
-  padding: 1rem 1rem;
+export const LogoArea = styled.div`
+  width: 20%;
+  height: 100%;
+  ${({ theme }) => theme.common.flexCenter};
 `;
-
-export const MenuIcons = styled.div`
-  margin: 0 10rem 0 0;
+export const Logo = styled.img.attrs({ src: 'img/HeaderLogo.png' })`
+  width: 15.6rem;
+  height: 7.5rem;
+  cursor: pointer;
+`;
+export const NavArea = styled.div`
+  width: 60%;
+  height: 100%;
   display: flex;
-  font-size: 2.4rem;
-
-  /* position: absolute;
-  right: 10%; */
+  justify-content: space-evenly;
+  align-items: center;
 `;
-export const IconList = styled.div`
-  padding: 0 1rem;
-  /* padding: 1rem 1rem; */
+export const Nav = styled.div`
+  ${({ theme }) => theme.common.flexCenter};
+  cursor: pointer;
+  padding: 2rem 3rem;
 `;
-
-//나중에 로그인버튼 바꿀떄
-export const SignInBtn = styled.button`
-  border-radius: 5rem;
-  border: 0.1rem solid #999999;
-  font-size: 2rem;
+export const UserContentArea = styled.div`
+  width: 20%;
+  height: 100%;
+  ${({ theme }) => theme.common.flexCenter};
+`;
+export const UserContentBox = styled.div`
+  width: 24rem;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+`;
+export const IconBox = styled.div`
+  width: 10.4rem;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+`;
+export const GreenBtn = styled.button`
+  width: 10.4rem;
   height: 4.8rem;
-  width: 10rem;
+  background-color: ${({ theme }) => theme.palette.green};
+  border-radius: 0.5rem;
+  font-size: 2rem;
+  font-weight: 500;
+  color: #ffffff;
+`;
+export const GrayBtn = styled(GreenBtn)`
+  background-color: #ffffff;
+  color: #999999;
+  border: 0.1rem solid #999999;
 `;
