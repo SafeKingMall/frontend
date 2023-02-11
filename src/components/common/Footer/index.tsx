5dbfd7fd9004028aa624861ed3eaa4d5a5b9a78b
이선우 <dkel3slek@naver.com>
2023-02-11 20:53:30 +0900

    design/Footer/#176

--- src/components/common/Footer/index.tsx
+++ src/components/common/Footer/index.tsx
@@ -3,19 +3,22 @@ import * as S from './style';
 
 export const Footer = () => {
   return (
-    <div>
-      <S.Wrapper>
-        <S.Logo>
-          <div></div>
-        </S.Logo>
-        <S.Text>
-          <span>㈜리크텍</span>
-          <br /> <span>Tel</span> (02) 6953-1170
-          <br /> <span>Address</span> (07802) 서울시 강서구 공항대로 213, 보타닉파크타워-2 522호
-          <br />
-          <br />ⓒ safekingmall All rights reserved.
-        </S.Text>
-      </S.Wrapper>
-    </div>
+    <S.container>
+      <S.LogoArea>
+        <S.Logo />
+      </S.LogoArea>
+      <S.TextArea>
+        <S.TextBox>
+          <div>
+            <p>(주)리크텍</p>
+            <p>Tel: (02) 6953-1170</p>
+            <p>Address: (07802) 서울시 강서구 공항대로 213, 보타닉파크타워-2 522호</p>
+          </div>
+          <div>
+            <p>ⓒ safekingmall All rights reserved.</p>
+          </div>
+        </S.TextBox>
+      </S.TextArea>
+    </S.container>
   );
 };
