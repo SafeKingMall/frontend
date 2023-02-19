9ec5ed049cf5c8cf30af5e6cd845b0a4de605794
이선우 <dkel3slek@naver.com>
2023-02-19 17:53:18 +0900

    design/ItemList/#208

--- src/components/item/List/style.ts
+++ src/components/item/List/style.ts
@@ -21,7 +21,7 @@ export const ScrollDiv = styled.div`
 `;
 export const ItemContainer = styled.div`
   cursor: pointer;
-  height: 46.5rem;
+  height: 50rem;
   width: 33.6rem;
   margin-right: 3.2rem;
   margin-bottom: 5.6rem;
@@ -34,17 +34,29 @@ export const ItemImg = styled.img`
   height: 33.6rem;
   border: 1px solid #dddddd;
 `;
+export const ItemTextArea = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  height: 14rem;
+`;
 export const Category = styled.p`
   font-size: 1.6rem;
-  margin-top: 2.4rem;
+  /* margin-top: 2.4rem; */
 `;
 export const ItemName = styled.p`
   font-size: 2.4rem;
   margin-top: 1rem;
+  width: 33.6rem;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  display: -webkit-box;
+  -webkit-line-clamp: 2;
+  -webkit-box-orient: vertical;
 `;
 export const Price = styled.p`
   font-size: 2rem;
-  margin-top: 2.6rem;
+  /* margin-top: 2.6rem; */
   color: ${(props) => props.theme.palette.txtgray};
 `;
 export const NoSearchItem = styled.div`
