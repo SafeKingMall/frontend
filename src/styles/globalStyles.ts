aa7bec1a25331e62da0eacf56197aaad2baf313d
이선우 <dkel3slek@naver.com>
2023-02-21 22:22:01 +0900

    design/Main/#233

--- src/styles/globalStyles.ts
+++ src/styles/globalStyles.ts
@@ -14,7 +14,7 @@ export default createGlobalStyle`
 }
 html {
   height: 100%;
-  font-size: 62.5%;
+  font-size: 625%;
   -webkit-text-size-adjust: none;
 }
 body {
@@ -57,67 +57,67 @@ input {
 }
 @media (max-width: 1560px) {
   html {
-    font-size: 57.5%;
+    font-size: 575%;
   }
 }
 @media (max-width: 1440px) {
   html {
-    font-size: 52.5%;
+    font-size: 525%;
   }
 }
 @media (max-width: 1320px) {
   html {
-    font-size: 47.5%;
+    font-size: 475%;
   }
 }
 @media (max-width: 1200px) {
   html {
-    font-size: 42.5%;
+    font-size: 425%;
   }
 }
 @media (max-width: 1080px) {
   html {
-    font-size: 37.5%;
+    font-size: 375%;
   }
 }
 @media (max-width: 960px) {
   html {
-    font-size: 32.5%;
+    font-size: 325%;
   }
 }
 @media (max-width: 840px) {
   html {
-    font-size: 27.5%;
+    font-size: 275%;
   }
 }
 @media (max-width: 720px) {
   html {
-    font-size: 27.5%;
+    font-size: 275%;
   }
 }
 @media (max-width: 600px) {
   html {
-    font-size: 22.5%;
+    font-size: 225%;
   }
 }
 @media (max-width: 480px) {
   html {
-    font-size: 17.5%;
+    font-size: 175%;
   }
 }
 @media (max-width: 360px) {
   html {
-    font-size: 12.5%;
+    font-size: 125%;
   }
 }
 @media (max-width: 240px) {
   html {
-    font-size: 7.5%;
+    font-size: 75%;
   }
 }
 @media (max-width: 120px) {
   html {
-    font-size: 2.5%;
+    font-size: 25%;
     min-width: 120px;
   }
 }
