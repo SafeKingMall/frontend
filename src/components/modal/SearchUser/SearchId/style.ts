3ddde298b897b5f118f390c9ca77064172587803
gyduddl <gyduddl4889@naver.com>
2023-02-22 18:28:56 +0900

    design/searchId/#240

--- src/components/modal/SearchUser/SearchId/style.ts
+++ src/components/modal/SearchUser/SearchId/style.ts
@@ -26,8 +26,10 @@ export const InputContainer = styled.div`
 `;
 export const InputWrapper = styled.div`
   height: 0.88rem;
-  border-bottom: 0.01rem solid #efeff1;
+  /* border-bottom: 0.01rem solid #efeff1; */
   position: relative;
+  bottom: 0.45rem;
+
   label {
     display: inline-block;
     width: 1.5rem;
@@ -50,10 +52,11 @@ export const InputWrapper = styled.div`
     position: absolute;
     top: 0.31rem;
     right: 1rem;
-    padding-top: 0.05rem;
+    padding-top: 0.4rem;
     margin-right: 0.2rem;
     font-size: 0.28rem;
     color: #a6a6a6;
+    border-bottom: 0.01rem solid #efeff1;
   }
   /* & > p {
     position: absolute;
@@ -66,30 +69,39 @@ export const InputWrapper = styled.div`
     content: '';
     position: absolute;
     left: 1.49rem;
-    top: 0.34rem;
+    top: 0.8rem;
     width: 0.01rem;
     background-color: #efeff1;
     width: 0.01rem;
     height: 0.24rem;
+  }
+  &::after {
+    content: '';
+    position: absolute;
+    right: 0;
+    top: 0.46rem;
+    width: 0.01rem;
+    background-color: #efeff1;
+    width: 5.92rem;
+    height: 0.01rem;
   }
 `;
 
 export const ErrMsg = styled.span`
   font-weight: 400;
   font-size: 0.14rem;
-  bottom: 4.3rem;
+  /* bottom: 4.3rem; */
+  top: 1.1rem;
   line-height: 0.19rem;
   color: #ff003e;
   position: absolute;
-  top: 0.64rem;
   left: 1.74rem;
 `;
 export const SearchBtn = styled.button`
-  margin: 0.48rem 0 0.48rem 0;
+  margin: 0.38rem 0 0.38rem 0;
   background-color: #289951;
   width: 3.12rem;
   height: 0.8rem;
-
   background: #289951;
   border-radius: 1rem;
   font-weight: 700;
@@ -126,7 +138,8 @@ export const TextWrapper = styled.div`
   }
 `;
 export const BtnWrapper = styled.div`
-  margin-top: 0.48rem;
+  margin-top: 0.2rem;
+  margin-bottom: 0.2rem;
   width: 5.92rem;
   display: flex;
   align-items: center;
@@ -171,13 +184,13 @@ export const SendBtn = styled.button`
   font-weight: 400;
   line-height: 0.19rem;
   position: absolute;
-  top: 0.25rem;
+  top: 0.7rem;
   right: 0.24rem;
 `;
 
 export const AuthTimer = styled.div`
   position: absolute;
-  top: 0.35rem;
+  margin-top: 0.1rem;
   right: 1.15rem;
 `;
 
@@ -192,6 +205,6 @@ export const CheckBtn = styled.button`
   font-weight: 400;
   line-height: 0.19rem;
   position: absolute;
-  top: 0.25rem;
+  top: 0.7rem;
   right: 0.24rem;
 `;
