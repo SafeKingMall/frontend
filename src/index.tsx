69c6c3bb13415fd280859ec214ce344c6a05dcda
이선우 <dkel3slek@naver.com>
2023-05-15 13:19:10 +0900

    feat/ReactSnap/#363

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,39 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+// import ReactDOM from 'react-dom/client';
+import { hydrate, render } from 'react-dom';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import ScrollToTop from './components/common/ScrollToTop';
 import { HelmetProvider } from 'react-helmet-async';
 
-const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
-root.render(
-  // <React.StrictMode>
-  <BrowserRouter>
-    <ScrollToTop />
-    <HelmetProvider>
-      <App />
-    </HelmetProvider>
-  </BrowserRouter>,
-  // {/* </React.StrictMode>, */}
-);
+// const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+// root.render(
+//   <BrowserRouter>
+//     <ScrollToTop />
+//     <HelmetProvider>
+//       <App />
+//     </HelmetProvider>
+//   </BrowserRouter>,
+// );
+const rootElement = document.getElementById('root') as HTMLElement;
+if (rootElement.hasChildNodes()) {
+  hydrate(
+    <BrowserRouter>
+      <ScrollToTop />
+      <HelmetProvider>
+        <App />
+      </HelmetProvider>
+    </BrowserRouter>,
+    rootElement,
+  );
+} else {
+  render(
+    <BrowserRouter>
+      <ScrollToTop />
+      <HelmetProvider>
+        <App />
+      </HelmetProvider>
+    </BrowserRouter>,
+    rootElement,
+  );
+}
