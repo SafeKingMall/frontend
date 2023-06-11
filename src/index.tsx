062d2cb2b0fc759fd48f81507ce60c14e80a2b85
이선우 <dkel3slek@naver.com>
2023-06-12 04:10:13 +0900

    feat/downgrade/#374

--- src/index.tsx
+++ src/index.tsx
@@ -1,53 +1,54 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+// import ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import ScrollToTop from './components/common/ScrollToTop';
 import { HelmetProvider } from 'react-helmet-async';
-// import { hydrate, render } from 'react-dom';
+import { hydrate, render } from 'react-dom';
 
-const container = document.getElementById('root') as HTMLElement;
-const root = ReactDOM.createRoot(container);
-if (container.hasChildNodes()) {
-  ReactDOM.hydrateRoot(
-    container,
-    <BrowserRouter>
-      <ScrollToTop />
-      <HelmetProvider>
-        <App />
-      </HelmetProvider>
-    </BrowserRouter>,
-  );
-} else {
-  root.render(
-    <BrowserRouter>
-      <ScrollToTop />
-      <HelmetProvider>
-        <App />
-      </HelmetProvider>
-    </BrowserRouter>,
-  );
-}
-
-// const rootElement = document.getElementById('root') as HTMLElement;
-// if (rootElement.hasChildNodes()) {
-//   hydrate(
+// const container = document.getElementById('root') as HTMLElement;
+// const root = ReactDOM.createRoot(container);
+// if (container.hasChildNodes()) {
+//   ReactDOM.hydrateRoot(
+//     container,
 //     <BrowserRouter>
 //       <ScrollToTop />
 //       <HelmetProvider>
 //         <App />
 //       </HelmetProvider>
 //     </BrowserRouter>,
-//     rootElement,
 //   );
 // } else {
-//   render(
+//   root.render(
 //     <BrowserRouter>
 //       <ScrollToTop />
 //       <HelmetProvider>
 //         <App />
 //       </HelmetProvider>
 //     </BrowserRouter>,
-//     rootElement,
 //   );
 // }
+
+// React17버전
+const rootElement = document.getElementById('root') as HTMLElement;
+if (rootElement.hasChildNodes()) {
+  hydrate(
+    <BrowserRouter>
+      <ScrollToTop />
+      <HelmetProvider>
+        <App />
+      </HelmetProvider>
+    </BrowserRouter>,
+    rootElement,
+  );
+} else {
+  render(
+    <BrowserRouter>
+      <ScrollToTop />
+      <HelmetProvider>
+        <App />
+      </HelmetProvider>
+    </BrowserRouter>,
+    rootElement,
+  );
+}
